7f3d2326df59839b0b97d7789c3fef8d9a4c386e
dchappell <douglas.arthur.chappell@gmail.com>
2019-05-17 20:45:54 -0400

    refactor home page view to use a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,29 +19,46 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists.
         self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
 
+        # She is invited to enter a to-do item straight away.
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual(
+            inputbox.get_attribute("placeholder"),
+            "Enter a to-do item"
+        )
 
-# She is invited to enter a to-do item straight away.
+        # She types "Buy peackock feathers" into a text box
+        # (Ediths hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
-# She types "Buy peackock feathers" into a text box
-# (Ediths hobby is tying fly-fishing lures)
+        # When she hits enter, the page updates and now the page lists.
+        # "1: Buy peacock feathers" as an item in a to-do list.
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# When she hits enter, the page updates and now the page lists.
-# "1: Buy peacock feathers" as an item in a to-do list.
+        table = self.browser.find_element_by_id("id_lsit_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows)
+        )
 
-# There is still a text box inviting her to add another item.
-# She enters "Use peacock feathers to make fly"
-# (Edith is very methodical)
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make fly"
+        # (Edith is very methodical)
 
-# The page updates again, and now shows both items on her list.
+        self.fail("finish the test!")
 
-# Edith wonders whether the site will remember her list.
-# Then she sees that the site has generated a unique URL for her
-# There is some explanatory text to that effect.
+        # The page updates again, and now shows both items on her list.
 
-# She visits that URL - her to-do list is still there.
+        # Edith wonders whether the site will remember her list.
+        # Then she sees that the site has generated a unique URL for her
+        # There is some explanatory text to that effect.
 
-# Satisfied, she goes back to sleep
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
 
 
 if __name__ == '__main__':
