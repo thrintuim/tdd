517ef663c5ffc197a34ad05c6ef149b2ea70a249
dchappell <douglas.arthur.chappell@gmail.com>
2019-08-12 20:10:03 -0400

    Made the lists fancy with some bootstrappy styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -7,7 +7,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -120,6 +120,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 786)
         time.sleep(1)
+        self.browser.set_window_size(1024, 786)
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
