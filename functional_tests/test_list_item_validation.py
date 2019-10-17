ec3f0ed5b6427f7025fbece308f671b1073b43a2
dchappell <douglas.arthur.chappell@gmail.com>
2019-10-16 20:14:21 -0400

    Adjust new list view to do model validation. Corrected punctuation in expected error message in FT.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -15,7 +15,7 @@ class ItemValidationTest(FunctionalTest):
         # that list items cannot be blank.
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
-            "You can't have an empty list item"
+            "You can't have an empty list item."
         )
         )
         # She tries again with some text for the item, which now works.
