96441a5b091b94ae1e733af6c1ad3c8e7f0c574d
dchappell <douglas.arthur.chappell@gmail.com>
2019-11-18 21:22:30 -0500

    Added form validation for an existing list.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.utils.html import escape
+from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
@@ -102,6 +103,20 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        print(f'this is the list id: {list1.id}')
+        item1 = Item.objects.create(list=list1, text="textey")
+        response = self.client.post(
+            f"/lists/{list1.id}/",
+            data={"text": "textey"}
+        )
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, "list.html")
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
